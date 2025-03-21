a84a03eafcbfc13c24b0158047c726c1eff45124
thegit-69 <robotzeror@gmail.com>
2025-03-21 23:25:01 +0530

    Update greatest of 2.c

--- decsion making/greatest of 2.c
+++ decsion making/greatest of 2.c
@@ -1,3 +1,22 @@
+/**************************************
+ * Program: 	greatest of 2
+ * By Author: 	DASARATH.C
+ **************************************/
+// A=10 B=9 C=8 D=7 F=0
+#include<stdio.h>
+void main()
+{
+	int a;
+	scanf("%d",&a);
+	switch(a)
+	{
+		case 10 :printf("A grade");break;
+		case 9  :printf("B grade");break;
+		case 8  :printf("C grade");break;
+		case 7  :printf("D grade");break;
+		default :printf("F grade");
+	}
+}
 #include<stdio.h>
 int main()
 {
