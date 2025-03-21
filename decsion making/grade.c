887aec708707356051c6b4b27ebabf8c97d436a4
thegit-69 <robotzeror@gmail.com>
2025-03-21 23:20:49 +0530

    Update grade.c

--- decsion making/grade.c
+++ decsion making/grade.c
@@ -1,3 +1,7 @@
+/**************************************
+ * Program: 	grade
+ * By Author: 	DASARATH.C
+ **************************************/
 // A=10 B=9 C=8 D=7 F=0
 #include<stdio.h>
 void main()
