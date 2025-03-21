4da9d6b924bdf0fb34d1868b65700e3cf6d03abf
thegit-69 <robotzeror@gmail.com>
2025-03-21 23:09:48 +0530

    Update celcius to far converter.c

--- operators and expressions/celcius to far converter.c
+++ operators and expressions/celcius to far converter.c
@@ -1,3 +1,8 @@
+/**************************************
+ * Date: 21-03-2025
+ * Program: celcius to far converter
+ * By Author: DASARATH.C
+ **************************************/
 #include<stdio.h>
 void main()
 {
