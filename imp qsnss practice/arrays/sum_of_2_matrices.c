b4af82843184a89894144f3e68b2afec5969ae99
DASARATH <robotzeror@gmail.com>
2025-03-31 17:41:51 +0530

    sum_of_2_matrices.c is uploaded

---
+++ imp qsnss practice/arrays/sum_of_2_matrices.c
@@ -0,0 +1,28 @@
+/**************************************
+ * Program: 	sum_of_2_matrices.c
+ * By Author: 	DASARATH.C
+ **************************************/
+#include<stdio.h>
+int main()
+{
+    int a[2][2]={{1,2},{3,4}};
+    int b[2][2]={{5,6},{7,8}};
+    int c[2][2];
+    int i,j;
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d ",c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
