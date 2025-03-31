f138407e5dac392624da49acd85df0ef0779210f
DASARATH <robotzeror@gmail.com>
2025-03-31 17:47:09 +0530

    subtract_2_matrices.c is uploaded

---
+++ imp qsnss practice/arrays/subtract_2_matrices.c
@@ -0,0 +1,28 @@
+/**************************************
+ * Program: 	subtract 2 matrices
+ * By Author: 	DASARATH.C
+ **************************************/
+#include<stdio.h>
+int main()
+{
+    int a[2][2]={{1,2},{3,4}};
+    int b[2][2]={{4,5},{6,7}};
+    int c[2][2];
+    int i,j;
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            c[i][j]=a[i][j]-b[i][j];
+        }
+    }
+    for(i=0;i<2;i++)
+    {
+        for(j=0;j<2;j++)
+        {
+            printf("%d",c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
